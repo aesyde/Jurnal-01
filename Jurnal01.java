787f3d1f6d72eadc87438627cc22751a5e65705d
aesyde <faruledo@gmail.com>
2023-09-27 09:32:36 +0700

    Kumpul jurnal 01

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main(String[] args){
+        System.out.println("NAMA: FAHRUL REDHO");
+        System.out.println("NIM: 607062300018");
+        System.out.println("KELAS: D3IF-47-04");
+    }
+}
